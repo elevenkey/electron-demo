3f0c3b204ad8522b0e69a60380cb62e78ca3dfb3
sunzhen <csunzhen@gmail.com>
2021-09-17 11:40:57 +0800

    test

--- preload.js
+++ preload.js
@@ -3,7 +3,7 @@
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
-    if (element) element.innerText = text
+    if (element) element.innerText = text + '-111'
   }
 
   for (const type of ['chrome', 'node', 'electron']) {
