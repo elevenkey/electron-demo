e91e63f0b56835c56877e7a03405ef0ea30acb52
sunzhen <csunzhen@gmail.com>
2021-09-18 15:59:02 +0800

    add autoreload

--- main.js
+++ main.js
@@ -1,43 +1,65 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+'use strict';
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+const {app, BrowserWindow} = require('electron');
+require('electron-reload')(__dirname)
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+// Standard stuff
+app.on('ready', () => {
+  let mainWindow = new BrowserWindow({width: 800, height: 600});
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-}
+  mainWindow.loadFile(`index.html`);
+  // the rest...
+});
+// // Modules to control application life and create native browser window
+// const {app, BrowserWindow} = require('electron')
+// const path = require('path')
+// const os = require('os')
+// const electronReload = require('electron-reload')
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+
+// function createWindow () {
+//   // Create the browser window.
+//   const mainWindow = new BrowserWindow({
+//     width: 800,
+//     height: 600,
+//     webPreferences: {
+//       nativeWindowOpen:true,
+//       preload: path.join(__dirname, 'preload.js')
+//     }
+//   })
+
+//   // and load the index.html of the app.
+//   mainWindow.loadFile(`index.html`)
+//   // Open the DevTools.
+//   // mainWindow.webContents.openDevTools()
+// }
+
+// // This method will be called when Electron has finished
+// // initialization and is ready to create browser windows.
+// // Some APIs can only be used after this event occurs.
+// app.whenReady().then(() => {
+//   createWindow()
   
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+//   app.on('activate', function () {
+//     // On macOS it's common to re-create a window in the app when the
+//     // dock icon is clicked and there are no other windows open.
+//     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+//   })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+//   // Standard stuff
+//   app.on('ready', () => {
+//     createWindow()
+//     // mainWindow.loadUrl(`index.html`);
+//     // the rest...
+//   });
+// })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// // Quit when all windows are closed, except on macOS. There, it's common
+// // for applications and their menu bar to stay active until the user quits
+// // explicitly with Cmd + Q.
+// app.on('window-all-closed', function () {
+//   if (process.platform !== 'darwin') app.quit()
+// })
+
+// // In this file you can include the rest of your app's specific main process
+// // code. You can also put them in separate files and require them here.
